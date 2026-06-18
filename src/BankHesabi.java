7e3e875ac64dca08e015fb0327ea90629bcfebcf
Fatima Jamalova <camalovafatime2003@gmail.com>
2026-06-18 17:58:38 +0400

    Java task_Bank Hesabı

---
+++ src/BankHesabi.java
@@ -0,0 +1,18 @@
+public class BankHesabi {
+    public static void main(String[] args) {
+
+        String musteriAdi = "Fatime";
+        int hesabNomresi = 123456;
+        double balans = 3842.50;
+        boolean hesabaktivdir = true;
+        String hesabNovu = "Əmanət";
+
+
+        System.out.println("Müştəri adı: " + musteriAdi);
+        System.out.println("Hesab nömrəsi: " + hesabNomresi);
+        System.out.println("Balans: " + balans);
+        System.out.println("Hesab aktiv mi? " + hesabaktivdir);
+        System.out.println("Hesab növü: " + hesabNovu);
+
+    }
+}
