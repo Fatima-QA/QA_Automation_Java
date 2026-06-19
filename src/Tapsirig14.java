585b0d0701074909dea23e22bfc7e5e4b32dc16f
Fatima Jamalova <camalovafatime2003@gmail.com>
2026-06-19 08:08:58 +0400

    Tapşırıq 14 (Ədədin xüsusiyyətləri) əlavə edildi

--- src/Tapsirig14.java
+++ src/Tapsirig14.java
@@ -17,6 +17,24 @@ public class Tapsirig14 {
         System.out.println("Həm 3-ə, həm də 7-yə bölünür? " + ucVeyeddiye);
         System.out.println("10-dan böyük, 100-dən kiçik? " + araliq);
 
+        int n1 = 105;
+
+        boolean cut1 = (n1 % 2 ==0 );
+        boolean musbet1 = (n1 > 0 );
+        boolean uceBolunur1 = (n1 % 3 ==0 );
+        boolean yeddiyeBolunur1 = ( n1 % 7 ==0);
+        boolean ucVeyeddiye1 = (n1 % 3 ==0 ) && (n1 % 7 ==0);
+        boolean araliq1 = ( n1 > 10 && n1 < 100);
+
+        System.out.println("n1 = " + n1);
+        System.out.println("Cüt? " + cut1);
+        System.out.println("Müsbət? " + musbet1);
+        System.out.println("3-ə bölünür? " + uceBolunur1);
+        System.out.println("7-yə bölünür? " + yeddiyeBolunur1);
+        System.out.println("Həm 3-ə, həm də 7-yə bölünür? " + ucVeyeddiye1);
+        System.out.println("10-dan böyük, 100-dən kiçik? " + araliq1);
+
+
 
 
 
