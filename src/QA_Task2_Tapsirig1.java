4a8172c0df69029d4571c0d694f1b2b58a2e3bf9
Fatima Jamalova <camalovafatime2003@gmail.com>
2026-06-24 14:53:16 +0400

    Xətalı kodlar düzəldildi

---
+++ src/QA_Task2_Tapsirig1.java
@@ -0,0 +1,9 @@
+public class QA_Task2_Tapşırıq1 {
+    public void main(String[] args){
+
+
+
+
+
+    }
+}
--- src/XetaliKod.java
+++ src/XetaliKod.java
@@ -1,11 +1,10 @@
 public class XetaliKod {
     public static void main(String[] args) {
-Int yas = 25;     //Xəta 1: İnt böyük hərflə başlamır, Java case-sensitive
-String sehir = baku;    //Xəta 2: String dəyərlər dırnaqda yazılır
-        double kredit mebleği = 5000;   //Xəta 3: double dan sonra tam adda boşluq olmaz
-        boolean aktiv = True;     // XƏta 4: True/false kiçik hərflə yazılır
-        final int LİMİT = 100;
-                LİMİT = 200;      // Xəta 5: final olduqdan sonra dəyər sabit qalır dəyişilə bilməz
-        System.out.println(yas).  // Xəta 6: hər kod sonrası nöqtəli vergül qoyulmalıdır
+int yas = 25;     //Xəta 1: İnt böyük hərflə başlamır, Java case-sensitive
+String sehir = "Baku";    //Xəta 2: String dəyərlər dırnaqda yazılır
+  double kreditMeblegi = 5000;   //Xəta 3: double dan sonra tam adda boşluq olmaz
+        boolean aktiv = true;     // XƏta 4: True/false kiçik hərflə yazılır // final int LİMİT = 100;
+        // LİMİT = 200;      // Xəta 5: final olduqdan sonra dəyər sabit qalır dəyişilə bilməz
+       System.out.println(yas); // Xəta 6: hər kod sonrası nöqtəli vergül qoyulmalıdır
 }
 }
