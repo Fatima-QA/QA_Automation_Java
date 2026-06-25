540545b6c22def93b0ae8b6277eeced81a7687ed
Fatima Jamalova <camalovafatime2003@gmail.com>
2026-06-26 03:07:52 +0400

    Swap — İki Dəyişənin Yerini Dəyiş

---
+++ src/Task3_Tapsirig4.java
@@ -0,0 +1,17 @@
+public class Task3_Tapsirig4 {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 40;
+        System.out.println("a= " +a + ", b= " + b);
+
+        int temp = a;
+        a = b;
+        b=temp;
+        System.out.println("a= " +a + ", b= " + b);
+
+
+
+
+    }
+}
+
